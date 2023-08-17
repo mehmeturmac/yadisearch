794f95d7d7c6060a3be35a6a9518a6e99d71b8bc
mehmeturmac <mehmeturmac@gmail.com>
2023-08-17 14:36:18 +0300

    contextApi added

---
+++ src/context/@types.main.tsx
@@ -0,0 +1,21 @@
+export interface IDisk {
+  public_url: string;
+  name: string;
+  status: string;
+}
+
+export interface Iitem {
+  name: string;
+  type: string;
+  size: string;
+  link: string;
+  virusStatus: string;
+}
+
+export type MainContextType = {
+  disks: IDisk[];
+  items: Iitem[];
+  loading: boolean;
+  saveDisk: (disk: IDisk) => void;
+  removeDisk: (public_url: string) => void;
+};
---
+++ src/context/mainContext.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React from 'react';
+import { IDisk, Iitem, MainContextType } from './@types.main';
+
+const local = localStorage.getItem('yadisearch');
+const localData = typeof local === 'string' ? JSON.parse(local) : null;
+
+export const MainContext = React.createContext<MainContextType | null>(null);
+
+export const MainProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [disks, setDisks] = React.useState<IDisk[]>(localData || []);
+  const [items, setItems] = React.useState<Iitem[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [filteredItems, setFilteredItems] = React.useState<Iitem[]>(items);
+
+  const saveDisk = (disk: IDisk) => setDisks([...disks, disk]);
+
+  const removeDisk = (public_url: string) => {
+    const newDisks = disks.filter((disk) => disk.public_url !== public_url);
+    setDisks(newDisks);
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem('yadisearch', JSON.stringify(disks));
+  }, [disks]);
+
+  // const filterItems = (search: string) => {
+  //   const filter = items.filter((item) => item.name.includes(search));
+  //   setFilteredItems(filter);
+  // };
+
+  const values = {
+    disks,
+    items,
+    loading,
+    saveDisk,
+    removeDisk,
+  };
+
+  return <MainContext.Provider value={values}>{children}</MainContext.Provider>;
+};
